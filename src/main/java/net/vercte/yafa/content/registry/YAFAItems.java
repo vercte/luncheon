f4607596093e64728a8e2f7d6dc60105aff6bbd3
Vercte <lopstedener@gmail.com>
2024-08-22 12:01:06 -0700

    initial commit

---
+++ src/main/java/net/vercte/yafa/content/registry/YAFAItems.java
@@ -0,0 +1,38 @@
+package net.vercte.yafa.content.registry;
+
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.tterrag.registrate.util.entry.ItemEntry;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.vercte.yafa.YAFA;
+import net.vercte.yafa.content.item.BaguetteItem;
+
+public class YAFAItems {
+    private static final CreateRegistrate REGISTRATE = YAFA.registrate();
+
+    public static final ItemEntry<BaguetteItem> BAGUETTE = REGISTRATE.item("baguette", BaguetteItem::new)
+            .properties(p -> p.food(
+                    makeFood(5, 0.2F, false)
+            ))
+            .register();
+
+    public static final ItemEntry<Item> ICE_CUBE = REGISTRATE.item("ice_cube", Item::new)
+            .properties(p -> p.food(
+                    makeFood(0,0, true)
+            ))
+            .register();
+
+
+    public static void register() {
+    }
+
+    public static FoodProperties makeFood(int nutrition, float saturationMod, boolean canAlwaysEat) {
+         FoodProperties.Builder foodProperties = new FoodProperties.Builder();
+                foodProperties.nutrition(nutrition);
+                foodProperties.saturationMod(saturationMod);
+                if(canAlwaysEat) {
+                    foodProperties.alwaysEat();
+                }
+         return foodProperties.build();
+    }
+}
---
+++ src/main/java/net/vercte/yafa/mixin/LivingEntityInvoker.java
@@ -0,0 +1,13 @@
+package net.vercte.yafa.mixin;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(LivingEntity.class)
+public interface LivingEntityInvoker {
+    @Invoker("addEatEffect")
+    public void addEatEffect(ItemStack itemStack, Level level, LivingEntity livingEntity);
+}
