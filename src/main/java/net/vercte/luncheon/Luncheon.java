6cb8d53d8cc596809fc8c689a399d94a742d331d
vercte <lopstedener@gmail.com>
2025-06-06 10:07:34 -0700

    initial setup

---
+++ src/main/java/net/vercte/luncheon/Luncheon.java
@@ -0,0 +1,15 @@
+package net.vercte.luncheon;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(Luncheon.ID)
+public class Luncheon {
+    public static final String ID = "luncheon";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Luncheon() {
+
+    }
+}
