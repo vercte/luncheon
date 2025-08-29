8a8efd20a0d00f1f5beaf6b622c8808287314ba5
vercte <lopstedener@gmail.com>
2025-08-28 23:16:56 -0700

    Yay! Yippee!

--- src/main/java/net/vercte/luncheon/content/processing/centrifugation/BasinCentrifugationMovementBehaviour.java
+++ src/main/java/net/vercte/luncheon/content/processing/centrifugation/BasinCentrifugationMovementBehaviour.java
@@ -7,12 +7,11 @@ import com.simibubi.create.content.processing.basin.BasinBlockEntity;
 import com.simibubi.create.content.processing.basin.BasinRecipe;
 import com.simibubi.create.foundation.recipe.RecipeConditions;
 import com.simibubi.create.foundation.recipe.RecipeFinder;
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.Direction;
 import net.minecraft.world.Container;
 import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.vercte.luncheon.LuncheonConfig;
-import net.vercte.luncheon.foundation.utility.LuncheonDebug;
 import net.vercte.luncheon.registry.LuncheonRecipeTypes;
 
 import java.util.*;
@@ -24,9 +23,13 @@ public class BasinCentrifugationMovementBehaviour {
     private static final String PROGRESS_TAG = "luncheon$centrifuge_progress";
 
     public static void tick(MovementContext context) {
-        if(context.world.isClientSide()) return;
         if(context.contraption instanceof BearingContraption bearing) {
             if(bearing.getFacing() != Direction.UP) return;
+
+            if(context.world.isClientSide()) {
+                int i = 0;
+                i += 0;
+            }
 
             double speed = Math.min(context.motion.length(), LuncheonConfig.maxCentrifugeSpeed);
 
@@ -42,8 +45,6 @@ public class BasinCentrifugationMovementBehaviour {
             int recipeProgress = newProgress;
             Optional<? extends Recipe<?>> foundRecipe = recipes.stream().filter(r -> {
                 if(r instanceof CentrifugationRecipe cRecipe) {
-                    if(Minecraft.getInstance().player != null)
-                        LuncheonDebug.actionbarMessage(String.format("progress %d, required %d", recipeProgress, cRecipe.getProcessingDuration()));
                     return cRecipe.getProcessingDuration() <= recipeProgress;
                 }
                 return false;
@@ -53,14 +54,25 @@ public class BasinCentrifugationMovementBehaviour {
                 Recipe<?> recipe = foundRecipe.get();
                 if(recipe instanceof CentrifugationRecipe cRecipe) {
                     newProgress -= cRecipe.getProcessingDuration();
-                    LuncheonDebug.actionbarMessage("Assembling!!", 0xffff00);
                     BasinRecipe.apply(basin, cRecipe);
                 }
             }
 
             basin.write(context.blockEntityData, false);
             context.data.putInt(PROGRESS_TAG, newProgress);
+            updateForClients(context);
         }
+    }
+
+    private static void updateForClients(MovementContext context) {
+        if(!context.world.isClientSide()) return;
+        BlockEntity beHere = context.contraption.presentBlockEntities.get(context.localPos);
+
+        BasinBlockEntity basin;
+        if(beHere instanceof BasinBlockEntity) basin = (BasinBlockEntity)beHere;
+        else return;
+
+        basin.load(context.blockEntityData);
     }
 
     private static BasinBlockEntity getBasin(MovementContext context) {
